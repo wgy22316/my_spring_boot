8927152881718dac74c5e43e5823d48c5dab00e3
wugy <wu.gongyao@puscene.com>
2017-09-18 20:33:35 +0800

    呵呵呵呵

---
+++ src/main/java/com/my_spring/repository/UserRepository.java
@@ -0,0 +1,39 @@
+package com.my_spring.repository;
+
+import com.my_spring.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserRepository(JdbcTemplate jdbcTemplate){
+
+        this.jdbcTemplate = jdbcTemplate;
+
+    }
+
+    /**
+     * 获取所有的用户
+     * @return
+     */
+    public List<User> usrList(){
+        return jdbcTemplate.query("select * from user", new RowMapper<User>() {
+
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                User user = new User();
+                user.setId(resultSet.getInt(1));
+                user.setUserName(resultSet.getString(2));
+                return user;
+            }
+        });
+    }
+}
